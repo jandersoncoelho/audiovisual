bbaed14ed6f4e22b33c6a9482e4dfbafd5bd89a7
Breno Parreira <breno.parreira@live.com>
2017-05-09 11:11:57 -0300

    2017-05-09_Tabela atualizada

--- script/audiovisual.sql
+++ script/audiovisual.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 04-Maio-2017 às 16:12
+-- Generation Time: 09-Maio-2017 às 16:10
 -- Versão do servidor: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -62,25 +62,17 @@ CREATE TABLE `emprestimos` (
   `nomeAtendente` varchar(255) NOT NULL,
   `numeroPatrimonio` varchar(255) NOT NULL,
   `nomeSolicitante` varchar(255) NOT NULL,
-  `nomeResponsavel` varchar(255) NOT NULL
+  `nomeResponsavel` varchar(255) NOT NULL,
+  `equipamento_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `emprestimos`
 --
 
-INSERT INTO `emprestimos` (`id`, `nomeDevolveu`, `dataRetirada`, `dataDevolucao`, `situacao`, `nomeAtendente`, `numeroPatrimonio`, `nomeSolicitante`, `nomeResponsavel`) VALUES
-(78, 'José Geraldo', '2017-05-03 10:28:00', '2017-05-04 09:32:00', 'Devolvido', 'Breno', '409098', 'João', 'Breno Parreira'),
-(79, 'Maria Joana', '2017-05-03 10:39:00', '2017-05-04 09:17:00', 'Devolvido', 'Breno', '545466', 'Janderson', 'Breno Parreira'),
-(80, 'Alberto', '2017-05-03 10:39:00', '2017-05-03 10:39:00', 'Devolvido', 'Breno', '54545454', 'Rodrigo', 'Breno'),
-(81, 'Maria', '2017-05-03 10:51:00', '2017-05-03 10:51:00', 'Devolvido', 'Breno', '323232', 'João', 'Breno'),
-(82, 'Marcos', '2017-05-03 16:49:00', '2017-05-03 19:56:00', 'Devolvido', 'Breno', '323232', 'Breno Parreira Gonçalves', 'Breno Parreira'),
-(83, 'Marcos', '2017-05-03 19:47:00', '2017-05-04 09:30:00', 'Devolvido', 'Breno Parreira', '2121212', 'Delfina', 'Breno Parreira'),
-(84, 'Antônio', '2017-05-03 20:00:00', '2017-05-04 09:22:00', 'Devolvido', 'Breno Parreira', '43434211', 'Fernanda', 'Breno Parreira'),
-(85, 'João', '2017-05-03 20:10:00', '2017-05-03 20:10:00', 'Devolvido', 'Breno Parreira', '34345345', 'Delfina', 'Breno Parreira'),
-(86, 'José', '2017-05-04 07:45:00', '2017-05-04 09:18:00', 'Devolvido', 'Breno Parreira', '45345322', 'Kurt Cobain', 'Breno Parreira'),
-(88, NULL, '2017-05-04 11:11:00', NULL, 'Pendente', 'Fulano', '2123211', 'Zeca Pagodinho', ''),
-(89, NULL, '2017-05-04 11:11:00', NULL, 'Pendente', 'Fulano', '565776876', 'Cristiano Ronaldo', '');
+INSERT INTO `emprestimos` (`id`, `nomeDevolveu`, `dataRetirada`, `dataDevolucao`, `situacao`, `nomeAtendente`, `numeroPatrimonio`, `nomeSolicitante`, `nomeResponsavel`, `equipamento_id`) VALUES
+(123, NULL, '2017-05-09 10:46:00', NULL, 'Pendente', 'Breno Parreira', '43343', 'Fernanda', '', 6),
+(124, NULL, '2017-05-09 11:06:00', NULL, 'Pendente', 'Breno Parreira', '212121', 'Zeca Pagodinho', '', 6);
 
 -- --------------------------------------------------------
 
@@ -98,21 +90,8 @@ CREATE TABLE `emprestimos_acessorios` (
 --
 
 INSERT INTO `emprestimos_acessorios` (`emprestimo_id`, `acessorio_id`) VALUES
-(78, 2),
-(78, 3),
-(79, 1),
-(80, 5),
-(82, 2),
-(83, 3),
-(84, 2),
-(84, 3),
-(84, 4),
-(85, 1),
-(86, 4),
-(86, 5),
-(88, 1),
-(89, 3),
-(89, 4);
+(123, 2),
+(124, 2);
 
 -- --------------------------------------------------------
 
@@ -123,19 +102,17 @@ INSERT INTO `emprestimos_acessorios` (`emprestimo_id`, `acessorio_id`) VALUES
 CREATE TABLE `equipamentos` (
   `id` int(11) NOT NULL,
   `nome` varchar(255) NOT NULL,
-  `numeroPatrimonio` varchar(255) NOT NULL,
   `created` datetime DEFAULT NULL,
-  `modified` datetime DEFAULT NULL
+  `modified` datetime DEFAULT NULL,
+  `numeroPatrimonio` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `equipamentos`
 --
 
-INSERT INTO `equipamentos` (`id`, `nome`, `numeroPatrimonio`, `created`, `modified`) VALUES
-(1, 'Notebook', '40203012', '2017-04-14 14:03:37', '2017-04-14 14:05:44'),
-(2, 'Trambolho', '23090909', '2017-04-14 14:04:30', '2017-04-14 14:04:30'),
-(5, 'Data Show', '21111223', '2017-05-04 11:42:45', '2017-05-04 11:42:45');
+INSERT INTO `equipamentos` (`id`, `nome`, `created`, `modified`, `numeroPatrimonio`) VALUES
+(6, 'Tablet', '2017-05-09 12:58:45', '2017-05-09 12:58:45', '98765432');
 
 -- --------------------------------------------------------
 
@@ -147,15 +124,6 @@ CREATE TABLE `equipamentos_acessorios` (
   `equipamento_id` int(11) NOT NULL,
   `acessorio_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `equipamentos_acessorios`
---
-
-INSERT INTO `equipamentos_acessorios` (`equipamento_id`, `acessorio_id`) VALUES
-(5, 3),
-(5, 5),
-(5, 6);
 
 -- --------------------------------------------------------
 
@@ -171,13 +139,6 @@ CREATE TABLE `ocorrencias` (
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `ocorrencias`
---
-
-INSERT INTO `ocorrencias` (`id`, `idEmprestimo`, `descricao`, `providenciaTomada`, `created`, `modified`) VALUES
-(1, 84, 'Faltou um teclado', 'Chamar a Polícia', '2017-05-03 23:05:13', '2017-05-03 23:05:13');
 
 -- --------------------------------------------------------
 
@@ -208,7 +169,14 @@ INSERT INTO `phinxlog` (`version`, `migration_name`, `start_time`, `end_time`, `
 (20170426103457, 'AddNomeResponsavelToEmprestimos', '2017-04-26 13:41:00', '2017-04-26 13:41:01', 0),
 (20170426104319, 'RemoveIdAtendenteFromEmprestimos', '2017-04-26 13:43:55', '2017-04-26 13:43:56', 0),
 (20170426104349, 'RemoveOcorrenciaFromEmprestimos', '2017-04-26 13:43:56', '2017-04-26 13:43:56', 0),
-(20170503215017, 'CreateSolicitantes', '2017-05-04 00:51:26', '2017-05-04 00:51:26', 0);
+(20170503215017, 'CreateSolicitantes', '2017-05-04 00:51:26', '2017-05-04 00:51:26', 0),
+(20170505135423, 'AddIdEquipamentoToEmprestimos', '2017-05-05 16:55:10', '2017-05-05 16:55:11', 0),
+(20170505154139, 'AddEquipamentoIdoToEmprestimos', '2017-05-05 18:41:51', '2017-05-05 18:41:51', 0),
+(20170509111944, 'AddEquipamentoIdToEmprestimos', '2017-05-09 14:22:16', '2017-05-09 14:22:17', 0),
+(20170509112805, 'AddEquipamentoIdIdexToEmprestimos', '2017-05-09 15:12:01', '2017-05-09 15:12:03', 0),
+(20170509114544, 'RemoveNumeroPatrimonioFromEquipamentos', '2017-05-09 14:45:56', '2017-05-09 14:45:56', 0),
+(20170509114739, 'AddNumeroPatrimonioToEquipamentos', '2017-05-09 14:47:51', '2017-05-09 14:47:52', 0),
+(20170509133554, 'AddAtendenteIdIndexToEmprestimos', '2017-05-09 16:39:01', '2017-05-09 16:39:03', 0);
 
 -- --------------------------------------------------------
 
@@ -236,7 +204,9 @@ INSERT INTO `solicitantes` (`id`, `nome`, `cpf`, `matricula`, `email`, `password
 (3, 'Fernanda', '0932323203', '434234321', 'fernanda@gmail.com', '$2y$10$Ih30k1P1uyFgLiOkcMry2eQVFsoM1fzimgqit9MiFnF7rF5MtJgZ.', '2017-05-03 22:41:22', '2017-05-03 22:41:22'),
 (4, 'Zeca Pagodinho', '23242422111', '3243234', 'zeca@gmail.com', '$2y$10$rIecO2pBhHcBFGQxyCK04OHo1ivkmVOp7nrE/jvG4mexDr2JbPiiG', '2017-05-04 10:43:30', '2017-05-04 10:43:30'),
 (5, 'Kurt Cobain', '832618188032', '4319486361', 'kurt@gmail.com', '$2y$10$6eCPAw3Szeqw7P1wCJBXTufv6Y4ggUtIaR67MQBVlnoAY4ndEiMO2', '2017-05-04 10:44:11', '2017-05-04 10:44:11'),
-(6, 'Cristiano Ronaldo', '932802421', '324343', 'real@mail.com', '$2y$10$4L2IHU2KZesgrhmjwebS8.WVPkEAbes/UJa66AZlzioRFWUlsOOmu', '2017-05-04 10:45:44', '2017-05-04 10:45:44');
+(6, 'Cristiano Ronaldo', '932802421', '324343', 'real@mail.com', '$2y$10$4L2IHU2KZesgrhmjwebS8.WVPkEAbes/UJa66AZlzioRFWUlsOOmu', '2017-05-04 10:45:44', '2017-05-04 10:45:44'),
+(7, 'Breno', '6564543424', '323232', 'breno140494@gmail.com', '$2y$10$myys4Utl/Xo8HEmkEaCICulpWYmPKW/f.j4CYpxxAjpkiDuKJUxJG', '2017-05-08 16:59:15', '2017-05-08 16:59:15'),
+(8, 'Clóvis', '9800992993', '827382', 'clovis@gmail.com', '$2y$10$nTDDOqFwfPnoKMUUi079AOQ8Fr6lYs1vfT36GGAjlEQHm/cXuYuwi', '2017-05-09 10:40:27', '2017-05-09 10:40:27');
 
 -- --------------------------------------------------------
 
@@ -259,12 +229,9 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `password`, `tipo`, `created`, `modified`) VALUES
-(9, 'Breno Parreira', 'breno.parreira@live.com', '$2y$10$7UG40dOn7Hw5BHEFZ4C4U.0OM4PIn9.yDSgxncrkni4Bj0grGLR2q', 'Administrador', '2017-05-03 22:23:45', '2017-05-03 22:23:45'),
-(10, 'Janderson', 'janderson@iftm.edu.br', '$2y$10$mJL/6DCImSvLQLB6bZO2nupxHapiZMyV8B71nEXgdRITVok7tU3SW', 'Administrador', '2017-05-03 22:24:19', '2017-05-03 22:24:19'),
-(11, 'Rodrigo', 'rodrigo@gmail.com', '$2y$10$/hlXopnsEEYiK7LHs4VMQuIbWQv9.WedWPkFfSTwv84wb5.SZ8sgm', 'Administrador', '2017-05-03 22:24:43', '2017-05-03 22:24:43'),
-(12, 'Márcio', 'marcio@gmail.com', '$2y$10$wk3Y2WDLhNQDwCV52OOBGuyrjz.aLXqL3tXYyMMegVpxcOKUbxhca', 'Atendente', '2017-05-03 22:25:15', '2017-05-03 22:25:15'),
-(13, 'Marcelo', 'marcelo@gmail.com', '$2y$10$aAAeB/hxjiQEOoGtb8gJoeYTTZ22Ed32vslazPk.Ayq1SwFSx344C', 'Atendente', '2017-05-04 10:41:40', '2017-05-04 10:41:40'),
-(14, 'Fulano', 'fulano@gmail.com', '$2y$10$RQycIWoOqwt2EGs4gYYEz.wTEESG4ljEklCmXxdqci9YqSEt9CaU6', 'Atendente', '2017-05-04 12:48:37', '2017-05-04 12:48:37');
+(15, 'Breno Parreira', 'breno.parreira@live.com', '$2y$10$TD2QomfLh1b4L4HuAJfH8ujYkD5CQtkUTCkH0mxZF12l5VxhGfYOy', 'Administrador', '2017-05-09 14:02:54', '2017-05-09 14:02:54'),
+(16, 'Rodrigo', 'rodrigo@gmail.com', '$2y$10$GXKzm1ty7ZsjxTCU9l3PzuIXb1K22srDbtSAhuG2vDQaUUp2o/CPG', 'Administrador', '2017-05-09 14:03:16', '2017-05-09 14:03:16'),
+(17, 'Márcio', 'marcio@gmail.com', '$2y$10$MITx2kkKznWlcwaZ1WhzaOzOoToUwQk2MIS3ccWl4NflEh7lxah8K', 'Atendente', '2017-05-09 14:03:30', '2017-05-09 14:03:30');
 
 --
 -- Indexes for dumped tables
@@ -280,7 +247,8 @@ ALTER TABLE `acessorios`
 -- Indexes for table `emprestimos`
 --
 ALTER TABLE `emprestimos`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `BY_EQUIPAMENTO_ID` (`equipamento_id`);
 
 --
 -- Indexes for table `emprestimos_acessorios`
@@ -293,8 +261,7 @@ ALTER TABLE `emprestimos_acessorios`
 -- Indexes for table `equipamentos`
 --
 ALTER TABLE `equipamentos`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `numeroPatrimonio` (`numeroPatrimonio`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `equipamentos_acessorios`
@@ -341,30 +308,36 @@ ALTER TABLE `acessorios`
 -- AUTO_INCREMENT for table `emprestimos`
 --
 ALTER TABLE `emprestimos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=125;
 --
 -- AUTO_INCREMENT for table `equipamentos`
 --
 ALTER TABLE `equipamentos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `ocorrencias`
 --
 ALTER TABLE `ocorrencias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `solicitantes`
 --
 ALTER TABLE `solicitantes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Limitadores para a tabela `emprestimos`
+--
+ALTER TABLE `emprestimos`
+  ADD CONSTRAINT `emprestimos_ibfk_1` FOREIGN KEY (`equipamento_id`) REFERENCES `equipamentos` (`id`);
 
 --
 -- Limitadores para a tabela `emprestimos_acessorios`
